998db6db53e0ae38f8a312bd52dad951fe94b323
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2021-08-20 22:31:48 +0530

    Add files via upload

---
+++ Javaall.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class Javaall {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		System.out.println("sakshi");//used to print
+//		int age=2;// 4bytes
+//		float avg=19.345f;
+//	
+//		long bignumber=45335647678l;
+//		double sak=454544.344534;
+//		boolean isValid=false ;
+//		//java has camel case 1st letter always small
+//		double sum= (double)sak/(double)avg;
+//		System.out.println(age++);
+//		System.out.println(age);
+//		System.out.println(++age);
+//		//taking user input
+//		System.out.println("enter your age ");
+//		Scanner sc= new Scanner(System.in);
+//		int yr=sc.nextInt();
+//		//String age=sc.nextLine(); for taking string input
+//		System.out.println(yr);
+//		int a=19;
+//		if(a>18) {System.out.println("u can vote");
+//		}
+////		else if(a=23) {System.out.println("ok");}
+//		else {System.out.println("u can not vote");}
+////		switch case
+//		char grade='A';
+//		switch(grade) {
+//		case 'A':
+//			System.out.println("u got 90%");
+//			break;
+//		case 'B':
+//			System.out.println("u got 80%");
+//			break;
+//		case 'C':
+//			System.out.println("u got 70%");
+//			break;
+//		case 'D':
+//			System.out.println("u got 60%");
+//			break;
+//		case 'E':
+//			System.out.println("u r fail");
+//			break;
+//		default:
+//			System.out.println("wrong i/p");
+//		
+//		}
+//	//logic operators &&,||,!
+//		int p = 32;
+//		if(p>18 && p<40) {System.out.println(" ya");
+//		}
+//		if(!(p>18)) {System.out.println(" ya");
+//		}
+////		loops for,while,do while
+//		for(int i=0; i<10;i++) {System.out.println(i);
+//	
+//	}
+//		int r=23;
+//		while(r<65) {
+//		if (r==45)continue;
+//		System.out.println(r);
+//		r++;
+//	
+//		}
+//		do {System.out.println(r);
+//		r++;}while(r<24);
+//		
+	//array
+		//int marks[]={23,24,25,26,27};
+	
+//	exeption handeling
+//		int a[]=new int[3];
+//		try {System.out.println(2/0);}
+//		//catch(ArrayIndexOutOfBoundException e) {System.out.println("ther is an error");
+//		System.out.println(e.getlocalizedMessage());}
+//		//u can also catch arithmatic exceptions by
+//		//cath(ArithmeticException e){}
+//		
+//	}
+//	//method or function
+//		//return type funcn_name(arguments){}
+//		static int avg(int no1,int no2)	{int sum= no1+no2;
+//		return sum/2;
+	}
+}
+	
+
